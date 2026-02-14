a9d40493a17bf47a38f7cdf5d706b8c26c3ed5a0
Jagappa <jagappamerigi@gmail.com>
2026-02-14 20:41:29 +0530

    off to date

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// 🔐 Any logged-in user
+router.get("/profile", protect, (req, res) => {
+  res.json({
+    message: "Profile accessed successfully",
+    user: req.user,
+  });
+});
+
+// 🔒 Admin-only route
+router.get("/admin", protect, adminOnly, (req, res) => {
+  res.json({
+    message: "Welcome Admin 🔥",
+    user: req.user,
+  });
+});
 
 module.exports = router;
